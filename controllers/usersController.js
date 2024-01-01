6dcc0d59857c7ace745c70b100a441a03e553fc3
Ferecus Tudor <tudorferecus@gmail.com>
2024-01-01 14:53:06 +0200

    v.1.2

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,7 @@ const register = async (req, res) => {
 
     let bodyName = req.body.name;
     let bodyMail = req.body.mail;
-    let bodyIP = req.socket.remoteAddress;
+    let bodyIP = req.socket.localAddress;
     let bodyPassword = req.body.password;
 
     console.log(bodyIP);
